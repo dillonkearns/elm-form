29529ec62c63b3f103ddb7370ad7adf9af3e4654
Dillon Kearns <dillon@incrementalelm.com>
2023-03-21 16:44:35 -0700

    Add prettier.

--- examples/main.js
+++ examples/main.js
@@ -2,12 +2,13 @@ import "./style.css";
 import { Elm } from "./src/Main.elm";
 
 if (process.env.NODE_ENV === "development") {
-  const ElmDebugTransform = await import("elm-debug-transformer")
+  const ElmDebugTransform = await import("elm-debug-transformer");
 
   ElmDebugTransform.register({
-    simple_mode: true
-  })
+    simple_mode: true,
+  });
 }
 
 const root = document.querySelector("#app div");
+
 const app = Elm.Main.init({ node: root });
